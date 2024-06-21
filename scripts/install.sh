01f8afb2e262294a6fd209f7655673537f566868
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-06-21 23:49:23 +0900

    refactor: インデント調整

--- scripts/install.sh
+++ scripts/install.sh
@@ -53,8 +53,8 @@ fi
 
 # Git
 if [ -n "$GIT_USER_NAME" ] && [ -n "$GIT_USER_EMAIL" ]; then
-    git config --global user.name "$GIT_USER_NAME"
-    git config --global user.email "$GIT_USER_EMAIL"
+  git config --global user.name "$GIT_USER_NAME"
+  git config --global user.email "$GIT_USER_EMAIL"
 fi
 
 # starship
