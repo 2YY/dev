5bdca3efe1ffd9cf98f4a34b0ded923884a053e4
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-06-22 01:04:52 +0900

    feat: Chocolatey をインストールする処理の追加

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -18,6 +18,9 @@ for ($i = 0; $i -lt $args.Length; $i++) {
 }
 
 # Chocolatey
+if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
 choco install ./configs/packages.config -y
 
 # Alacritty
