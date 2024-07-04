bf2be42884b5119e586d20b67ebbd190788634cb
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-07-04 22:56:19 +0900

    feat: ファイルの存在を予めチェックするように修正
    Mac で実行したとき、`/etc/os-release` がそもそも存在しなくてエラーメッセージが表示されてしまうのを防ぐため。

--- scripts/functions/is_ubuntu.sh
+++ scripts/functions/is_ubuntu.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# 関数: is_ubuntu
 # Ubuntuかどうかを判定する
 function is_ubuntu() {
-  if grep -qi 'ubuntu' /etc/os-release; then
+  if [ -f /etc/os-release ] && grep -qi 'ubuntu' /etc/os-release; then
     return 0
   else
     return 1
