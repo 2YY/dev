aa96d14f32f2f6cd7b76862ab5fae218d800a4fb
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-12-04 22:27:46 +0900

    feat: miseのインストール処理追加

--- scripts/install.sh
+++ scripts/install.sh
@@ -57,9 +57,17 @@ if is_ubuntu; then
 
   # fzf
   sudo apt install -y fzf
+
+  # mise
+  apt update -y && apt install -y gpg sudo wget curl
+  sudo install -dm 755 /etc/apt/keyrings
+  wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1> /dev/null
+  echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+  sudo apt update
+  sudo apt install -y mise
 else
   brew install jesseduffield/lazygit/lazygit
-  brew install bat ffmpeg fzf
+  brew install bat ffmpeg fzf mise
 fi
 brew bundle --global
 
