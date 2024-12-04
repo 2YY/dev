9cbe506c7c4eb0bb0fd4fa51c54da430cf062651
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-12-05 01:00:53 +0900

    feat: 不要なコマンドの削除

--- scripts/install.sh
+++ scripts/install.sh
@@ -86,7 +86,6 @@ if is_ubuntu; then
   # neovim
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
   chmod u+x nvim.appimage
-  ./nvim.appimage
   mkdir -p /opt/nvim
   mv -f nvim.appimage /opt/nvim/nvim
 
