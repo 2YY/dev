9feafeaa7b480b067a0aa3a9e92d04a638faaafa
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-12-04 11:44:27 +0900

    feat: lazygitのインストール処理修正

--- scripts/install.sh
+++ scripts/install.sh
@@ -40,7 +40,14 @@ if ! exists_command "brew"; then
     brew install gcc
   fi
 fi
-brew install jesseduffield/lazygit/lazygit
+if is_ubuntu; then
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf lazygit.tar.gz lazygit
+  sudo install lazygit -D -t /usr/local/bin/
+else
+  brew install jesseduffield/lazygit/lazygit
+fi
 brew bundle --global
 
 # Alacritty
