abb165001e7cf227ec12a268f6891c32c2d87a1c
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-12-04 11:58:35 +0900

    feat: batのインストール処理追加

--- scripts/install.sh
+++ scripts/install.sh
@@ -41,12 +41,19 @@ if ! exists_command "brew"; then
   fi
 fi
 if is_ubuntu; then
+  # lazygit
   LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
   curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
   tar xf lazygit.tar.gz lazygit
   sudo install lazygit -D -t /usr/local/bin/
+
+  # bat
+  sudo apt install -y bat
+  mkdir -p ~/.local/bin
+  ln -s /usr/bin/batcat ~/.local/bin/bat
 else
   brew install jesseduffield/lazygit/lazygit
+  brew install bat
 fi
 brew bundle --global
 
