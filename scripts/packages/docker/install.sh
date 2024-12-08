d6d41dae13abf813804b4acf69f7a946be994338
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-12-08 19:39:39 +0900

    feat: ubuntu でdockerのaptリポジトリを準備する処理の追加

--- scripts/packages/docker/install.sh
+++ scripts/packages/docker/install.sh
@@ -1,1 +1,16 @@
 brew install --cask rancher
+
+if is_ubuntu; then
+  sudo apt-get update
+  sudo apt-get install ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+else
+  brew install --cask rancher
+fi
