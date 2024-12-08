d1d41591176f1192b4848c1d0d43e8f61d144e18
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-12-08 17:01:22 +0900

    feat: アンインストールスクリプトの置き場所変更

--- scripts/commands/uninstall.sh
+++ scripts/commands/uninstall.sh
@@ -7,7 +7,7 @@ if [ -z "$1" ]; then
     error "パッケージ名が指定されていません。"
 fi
 
-PACKAGE_SCRIPT="scripts/uninstallers/$1.sh"
+PACKAGE_SCRIPT="scripts/packages/$1/uninstall.sh"
 file_exists "$PACKAGE_SCRIPT"
 
 bash "$PACKAGE_SCRIPT"
