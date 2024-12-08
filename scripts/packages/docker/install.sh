5fe4fb1fc78fe4777147866b840819eaeef14add
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-12-08 19:45:29 +0900

    feat: yオプション追加

--- scripts/packages/docker/install.sh
+++ scripts/packages/docker/install.sh
@@ -1,7 +1,7 @@
 if is_ubuntu; then
   # NOTE: apt のリポジトリを予めセットアップしておく
   sudo apt-get update
-  sudo apt-get install ca-certificates curl
+  sudo apt-get install -y ca-certificates curl
   sudo install -m 0755 -d /etc/apt/keyrings
   sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
   sudo chmod a+r /etc/apt/keyrings/docker.asc
