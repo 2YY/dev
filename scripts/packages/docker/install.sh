51ee02cba190f20cb9ad9caa216fa7454e8b0f29
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-12-18 12:21:50 +0900

    feat: ubuntu の Rancher Desktop インストール処理を追加

--- scripts/packages/docker/install.sh
+++ scripts/packages/docker/install.sh
@@ -4,6 +4,12 @@ if is_ubuntu; then
     echo 'insufficient privileges'; 
     sudo usermod -a -G kvm "$USER"; 
   }
+
+  # NOTE: Rancher Desktop のリポジトリを追加し、そこ経由で Rancher Desktop をインストール。
+  curl -s https://download.opensuse.org/repositories/isv:/Rancher:/stable/deb/Release.key | gpg --dearmor | sudo dd status=none of=/usr/share/keyrings/isv-rancher-stable-archive-keyring.gpg
+  echo 'deb [signed-by=/usr/share/keyrings/isv-rancher-stable-archive-keyring.gpg] https://download.opensuse.org/repositories/isv:/Rancher:/stable/deb/ ./' | sudo dd status=none of=/etc/apt/sources.list.d/isv-rancher-stable.list
+  sudo apt update
+  sudo apt install rancher-desktop
 else
   brew install --cask rancher
 fi
