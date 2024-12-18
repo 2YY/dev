01d074117430b85d45d97b7c7de94b3702a638da
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-12-18 12:17:34 +0900

    feat: ubuntu で rancher desktop の動作に必要な権限設定処理を追加

--- scripts/packages/docker/install.sh
+++ scripts/packages/docker/install.sh
@@ -1,17 +1,9 @@
 if is_ubuntu; then
-  # NOTE: apt のリポジトリを予めセットアップしておく
-  sudo apt-get update
-  sudo apt-get install -y ca-certificates curl
-  sudo install -m 0755 -d /etc/apt/keyrings
-  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-  sudo chmod a+r /etc/apt/keyrings/docker.asc
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt-get update
-  
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  # NOTE: 動作のために /dev/kvm に対する権限が必要。権限が無ければ設定しておく。
+  [ -r /dev/kvm ] && [ -w /dev/kvm ] || { 
+    echo 'insufficient privileges'; 
+    sudo usermod -a -G kvm "$USER"; 
+  }
 else
   brew install --cask rancher
 fi
