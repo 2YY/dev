0b98016d4e57db01f1d7438dee94a7de5f8a0190
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-12-18 19:05:00 +0900

    feat: mac用のアンインストール処理削除

--- scripts/packages/bat/ubuntu/uninstall.sh
+++ scripts/packages/bat/ubuntu/uninstall.sh
@@ -1,8 +1,4 @@
-if is_ubuntu; then
-  sudo apt remove -y bat
+sudo apt remove -y bat
 
-  # NOTE: インストール時に設定したシムリンクを削除しておく。
-  rm ~/.local/bin/bat
-else
-  brew uninstall bat
-fi
+# NOTE: インストール時に設定したシムリンクを削除しておく。
+rm ~/.local/bin/bat
