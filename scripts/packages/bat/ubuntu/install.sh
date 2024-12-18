a1cecf08e82c6aa1a9774e6d382eed8350c90c07
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-12-18 19:01:54 +0900

    feat: mac用のインストール処理削除

--- scripts/packages/bat/ubuntu/install.sh
+++ scripts/packages/bat/ubuntu/install.sh
@@ -1,9 +1,5 @@
-if is_ubuntu; then
-  sudo apt install -y bat
+sudo apt install -y bat
 
-  # NOTE: Ubuntu の場合、apt でインストールすると batcat というコマンド名になってしまうので、bat というコマンド名で使えるようにする。
-  mkdir -p ~/.local/bin
-  ln -s /usr/bin/batcat ~/.local/bin/bat
-else
-  brew install bat
-fi
+# NOTE: Ubuntu の場合、apt でインストールすると batcat というコマンド名になってしまうので、bat というコマンド名で使えるようにする。
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
