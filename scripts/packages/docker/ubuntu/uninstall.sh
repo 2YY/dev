4ee2d4b2ca2d23a36ddaa41f67f39d8ef653d3e5
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-12-18 21:48:03 +0900

    feat: インストール時に追加したリポジトリと鍵を削除する処理追加

--- scripts/packages/docker/ubuntu/uninstall.sh
+++ scripts/packages/docker/ubuntu/uninstall.sh
@@ -1,4 +1,7 @@
 sudo apt remove rancher-desktop
+sudo rm /etc/apt/sources.list.d/isv-rancher-stable.list
+sudo rm /usr/share/keyrings/isv-rancher-stable-archive-keyring.gpg
+sudo apt update
 
 # NOTE: インストール時に追加されたグループから脱退しておく。
 # HACK: `install.sh` から `kvm` グループへの追加処理自体が別パッケージに切り出された場合、ここも併せて修正。
