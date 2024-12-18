c28417f5d66e6f599872f18f5b2c7e220092226b
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-12-18 20:51:01 +0900

    feat: mac 用の処理を削除

--- scripts/packages/docker/ubuntu/install.sh
+++ scripts/packages/docker/ubuntu/install.sh
@@ -1,15 +1,11 @@
-if is_ubuntu; then
-  # NOTE: 動作のために /dev/kvm に対する権限が必要。権限が無ければ設定しておく。
-  [ -r /dev/kvm ] && [ -w /dev/kvm ] || { 
-    echo 'insufficient privileges'; 
-    sudo usermod -a -G kvm "$USER"; 
-  }
+# NOTE: 動作のために /dev/kvm に対する権限が必要。権限が無ければ設定しておく。
+[ -r /dev/kvm ] && [ -w /dev/kvm ] || { 
+  echo 'insufficient privileges'; 
+  sudo usermod -a -G kvm "$USER"; 
+}
 
-  # NOTE: Rancher Desktop のリポジトリを追加し、そこ経由で Rancher Desktop をインストール。
-  curl -s https://download.opensuse.org/repositories/isv:/Rancher:/stable/deb/Release.key | gpg --dearmor | sudo dd status=none of=/usr/share/keyrings/isv-rancher-stable-archive-keyring.gpg
-  echo 'deb [signed-by=/usr/share/keyrings/isv-rancher-stable-archive-keyring.gpg] https://download.opensuse.org/repositories/isv:/Rancher:/stable/deb/ ./' | sudo dd status=none of=/etc/apt/sources.list.d/isv-rancher-stable.list
-  sudo apt update
-  sudo apt install rancher-desktop
-else
-  brew install --cask rancher
-fi
+# NOTE: Rancher Desktop のリポジトリを追加し、そこ経由で Rancher Desktop をインストール。
+curl -s https://download.opensuse.org/repositories/isv:/Rancher:/stable/deb/Release.key | gpg --dearmor | sudo dd status=none of=/usr/share/keyrings/isv-rancher-stable-archive-keyring.gpg
+echo 'deb [signed-by=/usr/share/keyrings/isv-rancher-stable-archive-keyring.gpg] https://download.opensuse.org/repositories/isv:/Rancher:/stable/deb/ ./' | sudo dd status=none of=/etc/apt/sources.list.d/isv-rancher-stable.list
+sudo apt update
+sudo apt install rancher-desktop
